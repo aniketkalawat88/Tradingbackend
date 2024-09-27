60c2a602237a419e4ffde0675211a09b083f8f70
Aniket <aniketkalawat88@gmail.com>
2024-09-27 17:02:29 +0530

    bug

--- src/server.js
+++ src/server.js
@@ -14,30 +14,11 @@ const routerData = require("./router/route")
 require("./db/conn")
 
 
-// app.use(cors({
-//     origin: process.env.FRONTEND_URLS,
-//     credentials: true,
-// }));
-const corsOptions = {
-    origin: function (origin, callback) {
-      // Allow requests with no origin (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-  
-      if (frontendUrls.includes(origin)) {
-        // Origin is allowed
-        callback(null, true);
-      } else {
-        // Origin is not allowed
-        callback(new Error("CORS policy violation: Origin not allowed"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true, // Allow cookies and authorization headers
-  };
-
-app.use(cors(corsOptions));
-
+app.use(cors({
+    origin: "https://tradingbackend-1.onrender.com/api/topperformance",
+    methods:["GET","POST","PATCH","DELETE","PUT"],
+    credentials: true,
+}));
 
 
 app.use(cookieParser());
